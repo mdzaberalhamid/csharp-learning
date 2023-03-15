fe38cc56fc52cf671c6381804edbaccc39c8c91a
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-16 00:22:13 +0600

    Create 3_C#_Variables.cs file

---
+++ 3_C#_Variables.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // C# Variables
+        // int, double, char, string, bool
+
+        // Creating a string variable
+        string firstname = "Zaber";
+        Console.WriteLine(firstname);
+
+        // Creating int variables
+        int mynum1 = 25;
+        Console.WriteLine(mynum1);
+
+        int mynum2;
+        mynum2 = 5;
+        Console.WriteLine(mynum2);
+
+        int mynum3 = 1;
+        mynum3 = 2;  // mynum3 value is 2 now
+        Console.WriteLine(mynum3);
+
+        // Examples of other types
+        int i = 10;
+        Console.WriteLine(i);
+        double d = 3.1245;
+        Console.WriteLine(d);
+        char c = 'Z';
+        Console.WriteLine(c);
+        string msg = "Hello World!";
+        Console.WriteLine(msg);
+        bool b = true;
+        Console.WriteLine(b);
+    }
+}
