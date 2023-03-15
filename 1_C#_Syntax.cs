16ddf3ffdd431d4e0472970e18df3217e13d1a94
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-15 14:06:06 +0600

    Create 1_C#_Syntax.cs file

---
+++ 1_C#_Syntax.cs
@@ -0,0 +1,18 @@
+// For using classes from the System namespace
+using System;
+
+// Container for classes and other namespaces
+namespace HelloWorld
+
+{
+  // class is a container for data and methods which brings functionality
+  class Program {
+    
+    // The Main method, Any code inside it will be executed.
+    static void Main (String[] args) {
+      
+      // Console is a System namespace class which has WriteLine() to output or print text
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
