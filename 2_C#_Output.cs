6af5ed8fbe241a515742db2e743e1a84d14ead8f
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-16 00:16:28 +0600

    Create 2_C#_Output

---
+++ 2_C#_Output.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Output values or texts
+        Console.WriteLine("Hello World");
+
+        // Using more output methods
+        Console.WriteLine("Hi,");
+        Console.WriteLine("I am Zaber.");
+        Console.WriteLine("I am learning C#");
+
+        // Output numbers and do a simple math operation
+        Console.WriteLine(1);
+        Console.WriteLine(1 + 9);
+
+        // The WriteLine() outputs on a new line
+        // The Write() prints on the same line
+        Console.Write("Welcome, ");
+        Console.Write("Nice to meet you!");
+    }
+}
